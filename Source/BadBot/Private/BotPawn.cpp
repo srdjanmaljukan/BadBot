78c5cdb6702ca68a6609bedf92423715e84e5254
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2025-02-08 00:01:36 +0100

    Implement Enhanced Input movements

--- Source/BadBot/Private/BotPawn.cpp
+++ Source/BadBot/Private/BotPawn.cpp
@@ -2,33 +2,121 @@
 
 
 #include "BotPawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputAction.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
 
-// Sets default values
 ABotPawn::ABotPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BotMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BotMesh"));
+	SetRootComponent(BotMesh);
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->bEnableCameraLag = true;
+	CameraBoom->CameraLagSpeed = 30.f;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+
+	FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("FloatingPawnMovement"));
 }
 
-// Called when the game starts or when spawned
 void ABotPawn::BeginPlay()
 {
 	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+}
+
+void ABotPawn::Move(const FInputActionValue& Value)
+{
+	const FVector DirectionLocal = Value.Get<FVector>();
+
+	const FRotator Rotation = GetController()->GetControlRotation();
+	const FVector Forward = Rotation.Vector();
+	const FVector Right = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
+	const FVector Up = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Z);
+
+	FloatingPawnMovement->AddInputVector(Forward * DirectionLocal.X);
+	FloatingPawnMovement->AddInputVector(Right * DirectionLocal.Y);
+	FloatingPawnMovement->AddInputVector(Up * DirectionLocal.Z);
+}
+
+void ABotPawn::Look(const FInputActionValue& Value)
+{
+	const FVector2D Look = Value.Get<FVector2D>();
+	if (GetController())
+	{
+		AddControllerYawInput(Look.X);
+		AddControllerPitchInput(Look.Y);
+	}
+}
+
+void ABotPawn::Fire(const FInputActionValue& Value)
+{
+	// Add Flip Flop logic to toggle between firing and not firing
+	bool PlaceHolder = false;
+
+	if (Value.Get<bool>())
+	{
+		FireRifle(PlaceHolder);
+	}
+}
+
+void ABotPawn::FireRifle(bool IsLeftRifle)
+{
 	
 }
 
-// Called every frame
+FTransform ABotPawn::TraceForSpawnTranform(bool IsLeftRifle)
+{
+	const FVector TraceEndLocation = RifleTrace();
+
+	return GetSpawnTransform(TraceEndLocation, IsLeftRifle);
+}
+
+FVector ABotPawn::RifleTrace()
+{
+	return FVector();
+}
+
+FTransform ABotPawn::GetSpawnTransform(FVector TraceEnd, bool IsLeftRifle)
+{
+	return FTransform();
+}
+
 void ABotPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void ABotPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABotPawn::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABotPawn::Look);
 }
 
--- Source/BadBot/Public/BotPawn.h
+++ Source/BadBot/Public/BotPawn.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "BotPawn.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UFloatingPawnMovement;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class BADBOT_API ABotPawn : public APawn
@@ -12,18 +19,52 @@ class BADBOT_API ABotPawn : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ABotPawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Fire(const FInputActionValue& Value);
+
+	void FireRifle(bool IsLeftRifle);
+	FTransform TraceForSpawnTranform(bool IsLeftRifle);
+	FVector RifleTrace();
+	FTransform GetSpawnTransform(FVector TraceEnd, bool IsLeftRifle);
+
+	UPROPERTY(BlueprintReadWrite, Category = "Combat")
+	float Health = 100.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Combat")
+	FName SocketName;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	UStaticMeshComponent* BotMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	UFloatingPawnMovement* FloatingPawnMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookAction;
+
+private:
 
 };
